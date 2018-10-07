64e6723a3fe0f92a741365d1c27221c11d8c25e4
Edvin Felipe <eddigzol@gmail.com>
2018-10-07 12:45:37 -0600

    Inicializacion del proyecto

---
+++ src/interprete/Interprete.java
@@ -0,0 +1,17 @@
+package interprete;
+
+/**
+ *
+ * @author felipe
+ */
+public class Interprete {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+    }
+    
+}
